2981ae235030c8b910e2e9239fd04f46de0cecd7
Dirkhoogland <99063413@mydavinci.nl>
2022-03-23 12:05:18 +0100

    niet gebruikte usings weg gehaalt

--- OOP-PokeBattle/Pokemon.cs
+++ OOP-PokeBattle/Pokemon.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP_PokeBattle
+﻿namespace OOP_PokeBattle
 {
     class Pokemon
     {
